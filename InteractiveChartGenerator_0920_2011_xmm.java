82aa822e454baa60b7a27f1e8ac4d982b60d644c
SalvATore6163 <fickle_booger0n@icloud.com>
2025-09-20 13:11:54 +0100

    Configure JAVA 交互式图表生成器

---
+++ InteractiveChartGenerator_0920_2011_xmm.java
@@ -0,0 +1,20 @@
+// 代码生成时间: 2025-09-20 20:11:53
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+/**
+ * Interactive Chart Generator using Dropwizard
+ * This program generates interactive charts based on user input.
+ * 
+ * @author Your Name
+ * @version 1.0
+ */
+public class InteractiveChartGenerator {
+
+    // Define command line options
+    private static final String OPTION_DATA_FILE = 
